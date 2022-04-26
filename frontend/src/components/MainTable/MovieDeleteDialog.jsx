82b87ef741a883fd5e333c0c7999ba074e9592ad
Ervin Pangilinan <pangilinanervin22@gmail.com>
2022-04-26 23:32:33 +0800

    add mui dialog form and confirmation with react hook form

---
+++ frontend/src/components/MainTable/MovieDeleteDialog.jsx
@@ -0,0 +1,43 @@
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+} from "@mui/material";
+import React from "react";
+
+export default function MovieDeleteDialog({
+	currentObject,
+	handleOpenDialog,
+	handleDelete,
+	isOpen = false,
+}) {
+	return (
+		<>
+			<Dialog open={isOpen} onClose={handleOpenDialog}>
+				<DialogTitle>{"Confirmation the Action"}</DialogTitle>
+				<DialogContent sx={{ minWidth: "500px" }}>
+					<DialogContentText>
+						{"Are your sure you want delete this data "}
+						{currentObject.title}
+					</DialogContentText>
+					<DialogActions sx={{ mt: 2 }}>
+						<Button variant="contained" onClick={handleOpenDialog}>
+							Disagree
+						</Button>
+						<Button
+							variant="contained"
+							color="error"
+							onClick={handleDelete}
+							autoFocus
+						>
+							Agree
+						</Button>
+					</DialogActions>
+				</DialogContent>
+			</Dialog>
+		</>
+	);
+}
