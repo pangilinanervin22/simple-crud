b14ecb67ef8e7900563d6900feba85d6154c5ea4
Ervin Pangilinan <pangilinanervin22@gmail.com>
2022-04-26 23:40:58 +0800

    utils for maintable

---
+++ frontend/src/components/MainTable/utils/sortPath.js
@@ -0,0 +1,22 @@
+export function sortPath(array = [], path = "age", order = "asc") {
+	if (order !== "asc" && order !== "desc") {
+		console.error("Error asc or desc only for order params");
+		return array;
+	}
+
+	try {
+		array = array.sort(function (a, b) {
+			let x = a[path].toUpperCase(),
+				y = b[path].toUpperCase();
+			return x === y ? 0 : x > y ? 1 : -1;
+		});
+	} catch (e) {
+		array = array.sort(function (a, b) {
+			let x = a[path],
+				y = b[path];
+			return y === x ? 0 : y > x ? 1 : -1;
+		});
+	}
+
+	return order === "asc" ? array : array.reverse();
+}
