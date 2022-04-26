8b6bb15573ed5a2f592a904a142f8ede0eea949b
Ervin Pangilinan <pangilinanervin22@gmail.com>
2022-04-26 23:47:53 +0800

    add pages

---
+++ frontend/src/page/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export default function NotFound() {
+	return (
+		<>
+			<Typography variant="h1" align="center">
+				Not Found
+			</Typography>
+		</>
+	);
+}
---
+++ frontend/src/page/SecondTest.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SecondTest() {
+	return (
+		<>
+			<h2>secondTest</h2>
+		</>
+	);
+}
